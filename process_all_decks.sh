60c4a36978c4b4782c435306a77c468eec87714a
holbech <soeren.holbech@liveintent.com>
2022-06-23 11:19:05 +0200

    headline

--- process_all_decks.sh
+++ process_all_decks.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 NEEDS="$(ls *.dec | xargs -i $DIR/parse.py {})"
 
 HAVES="$( cat $1 | grep -v '^$' | awk -F $'\t' 'OFS=FS { print "-"$1,$2 }' )"
 
+echo "Count	Name	Rarity	Color(s)	Type(s)	Subtype(s)	Mana value	Mana cost	Price	Editions	IDs"
 echo "$NEEDS"$'\n'"$HAVES" | $DIR/enrich.py | sort -t $'\t' -k2 | $DIR/sum_field.py 0
 
 
